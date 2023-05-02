d944bdca1d48dea5fe42c3e8688a625d6fcf3f10
eyzim <60747028s@ntnu.edu.tw>
2023-05-02 23:11:14 +0800

    [feature] Add weather forecast from CWB in the following 6 hours. 1. Add weather board with weather service. 2. Bind images with the weather detail.

---
+++ src/app/weather-board/weather-board.component.ts
@@ -0,0 +1,9 @@
+import { WeatherCardComponent } from './../weather/weather-card/weather-card.component';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-weather-board',
+    templateUrl: './weather-board.component.html',
+    styleUrls: ['./weather-board.component.scss'],
+})
+export class WeatherBoardComponent {}
