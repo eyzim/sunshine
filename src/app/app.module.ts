30cd696d00a13c582f4986bde016857b72ba73db
eyzim <60747028s@ntnu.edu.tw>
2023-05-02 23:11:14 +0800

    [feature] Add weather forecast from CWB in the following 6 hours. 1. Add weather board with weather service. 2. Bind images with the weather detail.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather/weather-card/weather-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherBoardComponent } from './weather-board/weather-board.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, WeatherCardComponent, WeatherBoardComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
